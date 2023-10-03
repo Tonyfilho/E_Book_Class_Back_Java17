8c1d455d6c5fd57f42a22f21d90c66aec6762ac9
tonyfilho <oficinadosdrones@gmail.com>
2023-10-03 16:32:11 +0100

    add pdf template

--- tonyClass/src/app/java/java.module.ts
+++ tonyClass/src/app/java/java.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { JavaComponent } from './java.component';
 import { ReferenceXObjectXInstanceXClassComponent } from './ooP-Part01/reference-x-object-x-instance-x-class/reference-x-object-x-instance-x-class.component';
 import { RouterModule } from '@angular/router';
+import { StaticXInstanceComponent } from './ooP-Part01/static-x-instance/static-x-instance.component';
 
 const JAVA_ROUTER = [
   {path: "", component: JavaComponent},
   {path: "OOP-Part01", component: ReferenceXObjectXInstanceXClassComponent},
-  {path: "static-X-instance", component: ReferenceXObjectXInstanceXClassComponent}
+  {path: "static-X-instance", component: StaticXInstanceComponent}
 ];
 
 @NgModule({
   declarations: [
     JavaComponent,
-    ReferenceXObjectXInstanceXClassComponent
+    ReferenceXObjectXInstanceXClassComponent,
+    StaticXInstanceComponent
   ],
   imports: [
     CommonModule,
---
+++ tonyClass/src/app/java/ooP-Part01/static-x-instance/static-x-instance.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-static-x-instance',
+  templateUrl: './static-x-instance.component.html',
+  styleUrls: ['./../share-class.component.css']
+})
+export class StaticXInstanceComponent {
+
+}
