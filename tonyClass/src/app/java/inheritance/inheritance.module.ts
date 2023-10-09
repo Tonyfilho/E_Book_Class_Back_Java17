3e45af5b39786806e0d96103ad13311d5a2a84e0
tony_filho <antonio.filho@nexllence.com>
2023-10-09 10:23:47 +0100

    java_lang_Object

--- tonyClass/src/app/java/inheritance/inheritance.module.ts
+++ tonyClass/src/app/java/inheritance/inheritance.module.ts
@@ -5,11 +5,14 @@ import { RouterModule } from '@angular/router';
 import { Part01Component } from './part01/part01.component';
 import { Part02Component } from './part02/part02.component';
 import { Part03Component } from './part03/part03.component';
+import { JavaLangObjectComponent } from './java-lang-object/java-lang-object.component';
 
 const INHERITANCE_ROUTER = [
   {path: '', component: InheritanceComponent},
   {path: 'part01', component: Part01Component},
   {path: 'part02', component: Part02Component},
+  {path: 'part03', component: Part03Component },
+  {path: 'java-lang-object', component: JavaLangObjectComponent },
 ];
 
 @NgModule({
@@ -17,7 +20,8 @@ const INHERITANCE_ROUTER = [
     InheritanceComponent,
     Part01Component,
     Part02Component,
-    Part03Component
+    Part03Component,
+    JavaLangObjectComponent
   ],
   imports: [
     CommonModule,
---
+++ tonyClass/src/app/java/inheritance/java-lang-object/java-lang-object.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-java-lang-object',
+  templateUrl: './java-lang-object.component.html',
+  styleUrls: ['./../../share-class.component.css']
+})
+export class JavaLangObjectComponent {
+
+}
