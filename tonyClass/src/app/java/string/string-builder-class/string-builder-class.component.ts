75e683e6c8373b8bcd0f28714575f94a1f272aad
tonyfilho <oficinadosdrones@gmail.com>
2023-10-10 14:20:45 +0100

    string-builder-class.component

---
+++ tonyClass/src/app/java/string/string-builder-class/string-builder-class.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-string-builder-class',
+  templateUrl: './string-builder-class.component.html',
+  styleUrls: ['./../../share-class.component.css']
+})
+export class StringBuilderClassComponent {
+
+}
--- tonyClass/src/app/java/string/string.module.ts
+++ tonyClass/src/app/java/string/string.module.ts
@@ -5,12 +5,14 @@ import { StringComponent } from './string.component';
 import { RouterModule } from '@angular/router';
 import { AllAboutStringComponent } from './all-about-string/all-about-string.component';
 import { StringManipulationMethodsComponent } from './string-manipulation-methods/string-manipulation-methods.component';
+import { StringBuilderClassComponent } from './string-builder-class/string-builder-class.component';
 
 const JAVA_STRING_ROUTER = [
   {path: "", component: StringComponent},
   {path: "block-and-formatting", component: BlockAndFormattingComponent},
   {path: "all-about-the-string", component: AllAboutStringComponent},
   {path: "string-manipulation-methods", component: StringManipulationMethodsComponent},
+  {path: "string-builder-class", component: StringBuilderClassComponent},
 
 ];
 
@@ -19,7 +21,8 @@ const JAVA_STRING_ROUTER = [
     BlockAndFormattingComponent,
     StringComponent,
     AllAboutStringComponent,
-    StringManipulationMethodsComponent
+    StringManipulationMethodsComponent,
+    StringBuilderClassComponent
   ],
   imports: [
     CommonModule,
