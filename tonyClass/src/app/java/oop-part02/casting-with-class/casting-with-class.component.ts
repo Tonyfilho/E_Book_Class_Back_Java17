3d9352e0411e9eb043deed5ee3f4046120c4ab79
tonyfilho <oficinadosdrones@gmail.com>
2023-10-11 16:42:55 +0100

    casting-with-class.component

---
+++ tonyClass/src/app/java/oop-part02/casting-with-class/casting-with-class.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casting-with-class',
+  templateUrl: './casting-with-class.component.html',
+  styleUrls: ['./../../share-class.component.css']
+})
+export class CastingWithClassComponent {
+
+}
