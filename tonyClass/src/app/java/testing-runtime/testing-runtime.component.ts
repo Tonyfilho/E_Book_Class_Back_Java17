cc7fee538818d09955e91909f23b6bf98985c9b4
tony_filho <antonio.filho@nexllence.com>
2023-10-12 13:06:04 +0100

    fixing_directories

---
+++ tonyClass/src/app/java/testing-runtime/testing-runtime.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-testing-runtime',
+  templateUrl: './testing-runtime.component.html',
+  styleUrls: ['./../share-class.component.css']
+})
+export class TestingRuntimeComponent {
+
+}
