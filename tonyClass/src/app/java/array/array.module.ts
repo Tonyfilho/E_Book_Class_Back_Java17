4200ddaeea7d324f1aad21496ecccbe0d49de0bb
tony_filho <antonio.filho@nexllence.com>
2023-10-13 16:32:26 +0100

    array_util

--- tonyClass/src/app/java/array/array.module.ts
+++ tonyClass/src/app/java/array/array.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArrayComponent } from './array.component';
+import { JavaUtilArrayComponent } from './java-util-array/java-util-array.component';
 
 
 
 @NgModule({
   declarations: [
-    ArrayComponent
+    ArrayComponent,
+    JavaUtilArrayComponent
   ],
   imports: [
     CommonModule
