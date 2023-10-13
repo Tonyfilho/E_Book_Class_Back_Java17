455470a945f9edf57dc3e6a873c9fec6329ef209
tony_filho <antonio.filho@nexllence.com>
2023-10-13 14:19:40 +0100

    starting_array

---
+++ tonyClass/src/app/java/array/array.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-array',
+  templateUrl: './array.component.html',
+  styleUrls: ['./../../share_root.component.css']
+})
+export class ArrayComponent {
+
+}
---
+++ tonyClass/src/app/java/array/array.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArrayComponent } from './array.component';
+
+
+
+@NgModule({
+  declarations: [
+    ArrayComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ArrayModule { }
