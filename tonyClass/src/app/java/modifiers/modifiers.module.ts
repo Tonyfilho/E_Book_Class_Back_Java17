66a7b46a93def286fbd65c25280ef74bdaf129b5
tony_filho <antonio.filho@nexllence.com>
2023-10-23 09:43:39 +0100

    Interface_Part01

--- tonyClass/src/app/java/modifiers/modifiers.module.ts
+++ tonyClass/src/app/java/modifiers/modifiers.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ModifiersComponent } from './modifiers.component';
 import { AbstractComponent } from './abstract/abstract.component';
 import { RouterModule } from '@angular/router';
+import { InterfaceComponent } from './interface/interface.component';
 
 const JAVA_MODIFIER = [
   {path: "", component: ModifiersComponent},
   {path: "abstract", component: AbstractComponent},
+  {path: "interface", component: InterfaceComponent},
 
 ];
 
@@ -15,7 +17,8 @@ const JAVA_MODIFIER = [
 @NgModule({
   declarations: [
     ModifiersComponent,
-    AbstractComponent
+    AbstractComponent,
+    InterfaceComponent
   ],
   imports: [
     CommonModule,
