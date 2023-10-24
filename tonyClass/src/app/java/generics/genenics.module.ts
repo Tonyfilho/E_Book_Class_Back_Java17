a17be199aca2b7a536937ffcff56bbda29110da7
tony_filho <antonio.filho@nexllence.com>
2023-10-24 12:39:23 +0100

    comparable is done

--- tonyClass/src/app/java/generics/genenics.module.ts
+++ tonyClass/src/app/java/generics/genenics.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { GenenicsComponent } from './genenics.component';
 import { RouterModule } from '@angular/router';
 import { GenericsBasicsComponent } from './generics-basics/generics-basics.component';
+import { ComparableXComparatorComponent } from './comparable-x-comparator/comparable-x-comparator.component';
 
 const JAVA_GENERICS= [
   {path: "", component: GenenicsComponent},
   {path: "generics-basic", component: GenericsBasicsComponent},
+  {path: "comparable-x-comparator", component: ComparableXComparatorComponent},
  
 ];
 
@@ -14,7 +16,8 @@ const JAVA_GENERICS= [
 @NgModule({
   declarations: [
     GenenicsComponent,
-    GenericsBasicsComponent
+    GenericsBasicsComponent,
+    ComparableXComparatorComponent
   ],
   imports: [
     CommonModule,
