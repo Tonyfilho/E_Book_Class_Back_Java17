0e75570b3dc6f798739080bfeaa713a5c9149157
tony_filho <antonio.filho@nexllence.com>
2023-11-14 10:04:25 +0000

    String_Immutable

---
+++ tonyClass/src/app/java/java_Certification/string/String_Manipulating_Immutable.java
@@ -0,0 +1,21 @@
+package app.java.java_Certification.string;
+
+public class String_Manipulating_Immutable {
+
+
+
+    static String s;
+    public static void main(String[] args) {
+
+       String s;
+       boolean isOk=true;
+       if (isOk) s = String_Manipulating_Immutable.s+"";  // Line 1
+       else s="";
+       s.concat("Hello ");  // Line 2
+       s.concat("World");
+       s.toUpperCase();
+       System.out.println("s = "+s);
+
+    }
+}
+
--- tonyClass/src/app/java/java_Certification/string/String_Manitpulation.java
+++ tonyClass/src/app/java/java_Certification/string/String_Manitpulation.java
@@ -1,17 +1,19 @@
 package app.java.java_Certification.string;
 
+
 public class String_Manitpulation {
 
-  public static void main(String... args) { // Line 1
+  public static void main(String ...args) { // Line 1
 
     String hello = "hello";
+   // System.out.println("ArrayArgs: " + args[0]);
 
     for (var arg : args) { // Line 2
-      System.out.println("ArrayArgs: " + args);
-      StringBuilder sb = new StringBuilder(arg); // Line 3
+      System.out.println("ArrayArgs: " + arg);
+      String sb = new String(arg);//   StringBuilder sb = new StringBuilder(arg); // Line 3
       System.out.print(arg.equals(sb) + " "); // Line 4
-      System.out.print((hello.equals(arg)) + " "); // Line 5
-      System.out.println((hello == arg)); // Line 6
+       System.out.print((hello.equals(arg)) + " "); // Line 5
+      System.out.println( (hello==arg.intern())); // Line 6 System.out.println((hello == arg)); // Line 6
 
     }
   }
